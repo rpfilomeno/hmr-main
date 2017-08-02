63fb385e258808460f0d807cec5d7d8d49d6fba8
dakkiboy17 <dakkiboy17@gmail.com>
2017-08-02 11:43:31 +0800

    handle null values

--- src/hmr/com/bean/Auction.java
+++ src/hmr/com/bean/Auction.java
@@ -231,18 +231,26 @@ public class Auction extends SystemBean {
 	}
 	
 	private static String escapeHTML(String s) {
-	    StringBuilder out = new StringBuilder(Math.max(16, s.length()));
-	    for (int i = 0; i < s.length(); i++) {
-	        char c = s.charAt(i);
-	        if (c > 127 || c == '"' || c == '<' || c == '>' || c == '&') {
-	            out.append("&#");
-	            out.append((int) c);
-	            out.append(';');
-	        } else {
-	            out.append(c);
-	        }
-	    }
-	    return out.toString();
+		
+		if(s!=null){
+			
+		    StringBuilder out = new StringBuilder(Math.max(16, s.length()));
+		    for (int i = 0; i < s.length(); i++) {
+		        char c = s.charAt(i);
+		        if (c > 127 || c == '"' || c == '<' || c == '>' || c == '&') {
+		            out.append("&#");
+		            out.append((int) c);
+		            out.append(';');
+		        } else {
+		            out.append(c);
+		        }
+		    }
+		    return out.toString();
+			
+		}else{
+			return null;
+		}
+		
 	}
 
 }
